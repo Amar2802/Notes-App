e60bdc0ecc3c86ba59dc0b88f60297fb88c94c00
Amarnath Yadav <Amar2822@outlook.com>
2026-06-02 09:24:52 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -6,10 +6,11 @@ from schemas import UserCreate, UserLogin, NoteCreate
 from auth import (hash_password, verify_password, create_access_token, verify_token)
 
 Base.metadata.create_all(bind=engine)
+
 def get_current_user(authorization: str = Header(None)):
     if not authorization:
         raise HTTPException(status_code=401, detail="Authorization header missing")
-    token = authorization.split(" ")[1] if " " in authorization else authorization
+    token = authorization.replace("Bearer ", "")
     payload = verify_token(token)
     if not payload:
         raise HTTPException(status_code=401, detail="Invalid token")
@@ -25,7 +26,7 @@ def home():
 def register(user: UserCreate, db: Session = Depends(get_db)):
     existing_user = db.query(User).filter((User.username == user.username) | (User.email == user.email)).first()
     if existing_user:
-        return {"error": "Email already exists"}
+        raise HTTPException(status_code=400, detail="Email already exists")
     new_user = User(username=user.username, email=user.email, password=hash_password(user.password))
     db.add(new_user)
     db.commit()
@@ -35,9 +36,15 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
     if not db_user:
-        return {"error": "Invalid email or password"}
+        raise HTTPException(
+    status_code=401,
+    detail="Invalid email or password"
+)
     if not verify_password(user.password, db_user.password):
-        return {"error": "Invalid email or password"}
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
     token = create_access_token({"user_id": db_user.id})
     return {"access_token": token}
 
